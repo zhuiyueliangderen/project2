92df8c8db92b2a10b779baeeca727ca1d331b6cd
LiuDi <1325563101@qq.com>
2019-03-04 20:54:30 +0800

    adding files

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ .mui-bar-tab .mui-tab-item-tao.mui-active {
+    color: #007aff;
+ }
+.mui-bar-tab .mui-tab-item-tao {
+    display: table-cell;
+    overflow: hidden;
+    width: 1%;
+    height: 50px;
+    text-align: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: #929292;
+}
+.mui-bar-tab .mui-tab-item-tao .mui-icon {
+    top: 3px;
+    width: 24px;
+    height: 24px;
+    padding-top: 0;
+    padding-bottom: 0;
+}
+
+.mui-bar-tab .mui-tab-item-tao .mui-icon~.mui-tab-label {
+   font-size:11px;
+   display:block;
+   overflow:hidden;
+   text-overflow:ellipsis;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//1.引入mint-ui样式文件
+import 'mint-ui/lib/style.css'
+Vue.config.productionTip = false
+//2.引入mui库样式文件
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
